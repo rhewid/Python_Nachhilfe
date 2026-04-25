2742862ea54e18993b5066dc4cfcd6a6ae49c1b1
rhewid <rhett.w@web.de>
2026-04-25 15:08:26 +0200

    Update aufgabe2_schleifen.py

--- Aufgaben/aufgabe2_schleifen.py
+++ Aufgaben/aufgabe2_schleifen.py
@@ -1,11 +1,13 @@
 # Aufgabe 1: For-Schleife
 # Gib die Zahlen von 1 bis 10 mit einer for-Schleife aus.
+# Bitte zeige mir zwei mögliche Implementierungen hierfür. 
+# Für die erste nutze range, für die zweite addiere eins dazu, da schleifenindexe bei 0 beginnen.
 
 # Schreibe deinen Code hier:
 
 
 # Aufgabe 2: While-Schleife
-# Gib die Zahlen von 10 bis 1 mit einer while-Schleife aus.
+# Gib die Zahlen von 10 bis 1 mit einer while-Schleife aus. Also 10, 9, 8, 7
 
 # Schreibe deinen Code hier:
 
