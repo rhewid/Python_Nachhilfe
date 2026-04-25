bd82a172f542bebd2b063faf48341e16aa815d3a
rhewid <rhett.w@web.de>
2026-04-25 15:00:19 +0200

    Update aufgabe1_grundlagen.py

--- Aufgaben/aufgabe1_grundlagen.py
+++ Aufgaben/aufgabe1_grundlagen.py
@@ -14,6 +14,8 @@
 
 # Aufgabe 3: Einfache Berechnung
 # Berechne das Alter in Tagen, wenn du das Alter in Jahren hast.
-# (Annahme: 365 Tage pro Jahr)
+# (Annahme: 365 Tage pro Jahr) 
+# Erstelle eine Variable die einen input vom typ int akzeptiert.
+# Erstelle eine weitere Variable die das eingegebene Alter mit 365 multipliziert und gebe dies in einer print Funktion aus.
 
 # Schreibe deinen Code hier:
