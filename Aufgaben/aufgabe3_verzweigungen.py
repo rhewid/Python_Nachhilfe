bd4d1b5287659faf8af6e91066db44c9ec6c7ebc
rhewid <rhett.w@web.de>
2026-04-25 15:11:37 +0200

    Update aufgabe3_verzweigungen.py

--- Aufgaben/aufgabe3_verzweigungen.py
+++ Aufgaben/aufgabe3_verzweigungen.py
@@ -1,17 +1,18 @@
 # Aufgabe 1: Einfache Verzweigung
 # Frage nach dem Alter und gib aus, ob die Person volljährig ist (18+).
+# Erzeuge eine Variable, die das Alter über die input Funktion abfrägt und nur Zahlen zulässt. In einer if-Verzweigung prüfen ob 18 Jahre oder älter.
 
 # Schreibe deinen Code hier:
 
 
 # Aufgabe 2: Mehrere Bedingungen
-# Frage nach einer Note (1-6) und gib das entsprechende Prädikat aus.
+# Frage nach einer Note (1-6) und gib das entsprechende Prädikat aus. Denke hier an if, elif, else
 # 1: Sehr gut, 2: Gut, 3: Befriedigend, 4: Ausreichend, 5: Mangelhaft, 6: Ungenügend
 
 # Schreibe deinen Code hier:
 
 
 # Aufgabe 3: Gerade oder Ungerade
-# Frage nach einer Zahl und gib aus, ob sie gerade oder ungerade ist.
+# Frage nach einer Zahl und gib aus, ob sie gerade oder ungerade ist. Nutze hierfür in der Abfrage die "Modulo Funktion" % 2 == 0. Als Beispiel in Sprache: Wenn meine Zahl  % 2 == 0 ist, ist sie gerade, sonst ungerade
 
 # Schreibe deinen Code hier:
