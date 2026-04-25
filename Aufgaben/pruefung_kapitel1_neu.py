ba22b9260d5c50e6fba2a700d71915acbc9f19e7
rhewid <rhett.w@web.de>
2026-04-25 15:16:19 +0200

    Update pruefung_kapitel1_neu.py

--- Aufgaben/pruefung_kapitel1_neu.py
+++ Aufgaben/pruefung_kapitel1_neu.py
@@ -6,17 +6,21 @@
 
 # Antwort:
 
+# Was gibt print("Ich bin " + alter + " Jahre alt.") aus?
 
-# Frage 2: Schreibe Code, um eine Farbe einzulesen und "Deine Lieblingsfarbe ist [Farbe]." auszugeben.
+# Antwort:
 
-# Code:
-
-
-# Frage 3: Berechne das Doppelte einer eingelesenen Zahl.
+# Frage 2: Schreibe Code, um eine Farbe einzulesen und "Deine Lieblingsfarbe ist [Farbe]." auszugeben. Nutze input.
 
 # Code:
 
 
-# Frage 4: Gib deinen Namen und dein Alter in einem Satz aus, ohne input.
+# Frage 3: Berechne das Doppelte einer eingelesenen Zahl. Nutze input um eine Zahl einzulesen, es dürfen nur Zahlen eingelesen werden.
+
+# Code:
+
+
+# Frage 4: Erstelle Variablen für Name und Alter, wobei Alter ein int sein soll. 
+# Gib deinen Namen und dein Alter in einem Satz aus, ohne input - einfach in einem print()
 
 # Code:
